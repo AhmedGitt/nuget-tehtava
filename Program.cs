cde6fbdcf42aa0414049e3e734da1684b77bed95
Ahmed <ahmed.alhassan97@hotmail.com>
2022-10-05 13:58:12 +0300

    nuget commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        for (int i = 1; i <= 3; i++)
+        {
+            var name = Faker.Name.FullName();
+            var city = Faker.Address.City();
+            var id = Faker.Identification.UkNationalInsuranceNumber();
+
+            Console.WriteLine($"Hello {name} from {city} Identification number {id}");
+        }
+    }
+}
